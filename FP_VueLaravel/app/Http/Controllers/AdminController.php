b4735064e698a0d31bf387ec477f9d98241f96e6
Francisco Pinto <fran021@gmail.com>
2022-11-18 03:16:50 -0400

    Adding MySQL

--- FP_VueLaravel/app/Http/Controllers/AdminController.php
+++ FP_VueLaravel/app/Http/Controllers/AdminController.php
@@ -8,10 +8,40 @@ class AdminController extends Controller
 {
     public function adminview(){
         $param = [
-            'param 1'=>'21',
-            'param 2'=>'45',
-            'param 3'=>'34'
+            'param1'=>'21',
+            'param2'=>'45',
+            'param3'=>'34',
+            'param4'=>null
         ];
-        return view('app.admin.admin', compact('param'));
+
+
+        $fornecedores = [
+            0 => [
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '0',
+                'ddd' => '', //São Paulo (SP)
+                'telefone' => '0000-0000'
+            ],
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '85', //Fortaleza (CE)
+                'telefone' => '0000-0000'
+            ],
+            2 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '32', //Juiz de fora (MG)
+                'telefone' => '0000-0000'
+            ]
+        ];
+
+
+        echo (isset($param)) ? "Setado" : "Vazio"; 
+
+        return view('app.admin.admin', compact('param'), compact('fornecedores'));
     }
 }
--- FP_VueLaravel/resources/views/app/admin/admin.blade.php
+++ FP_VueLaravel/resources/views/app/admin/admin.blade.php
@@ -7,10 +7,122 @@ echo 'hello';
 
 @endphp
 
-@if(count($param) > 0 && count($param) < 10)
-    <h2>menos de 10</h2>
-@elseif(count($param) > 10)
-    <h2>mais de 10</h2>
-@else
-    <h2>nenhum</h2>
-@endif
+{{'Test String'}}
+
+<br>
+<br>
+
+@isset($param)
+
+    @if(count($param) > 0 && count($param) < 10)
+        <h2>menos de 10</h2>
+    @elseif(count($param) > 10)
+        <h2>mais de 10</h2>
+    @else
+        <h2>nenhum</h2>
+    @endif
+
+        @isset($param)
+        esta aqui o parametro
+        @endisset
+    <br>
+    {{-- unless --}}
+    @unless(count($param) > 10)
+        Usando Unless
+    @endunless
+
+@endisset
+
+{{-- 
+    empty variable is:
+    - ''
+    - 0
+    - 0.0
+    - '0'
+    - null
+    - false
+    - array()
+    - $var
+--}}
+<br>
+
+@empty($param['param3'])
+- vazio
+@endempty
+
+
+<br>
+{{ $param['param1'] }}
+<br>
+{{ $param['param2'] }}
+<br>
+{{ $param['param3'] }}
+<br>
+{{ $param['param4'] ?? 'Nao informado'}}
+
+
+<br>
+<br>
+
+@foreach ($param as $row )
+    <h2>{{ $row ?? 'Vazio' }}</h2>
+@endforeach
+
+
+@switch($param['param4'])
+    @case(null)
+        Invalido
+        @break
+    @default
+        Algum valor
+@endswitch        
+
+<br>
+<br>
+
+@php $i = 0 @endphp
+@while(isset($fornecedores[$i]))
+    Fornecedor: {{ $fornecedores[$i]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[$i]['status'] }}
+    <br>
+    CNPJ: {{ $fornecedores[$i]['cnpj'] ?? '' }}
+    <br>
+    Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
+    <hr>
+    @php $i++ @endphp
+@endwhile
+
+@{{$fornecedores}}
+
+
+<br>
+<br>
+@forelse($fornecedores as $indice => $fornecedor)
+
+        Iteração atual: {{ $loop->iteration }}
+        <br>
+        Fornecedor: {{ $fornecedor['nome'] }}
+        <br>
+        Status: {{ $fornecedor['status'] }}
+        <br>
+        CNPJ: {{ $fornecedor['cnpj'] ?? '' }}
+        <br>
+        Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+        <br>
+        @if($loop->first)
+            Primeira iteração no loop
+
+
+        @endif
+
+        @if($loop->last)
+            Última iteração no loop
+            <br>
+            Total de registros: {{ $loop->count }}
+        @endif
+        <hr>
+    @empty
+        Não existem fornecedores cadastrados!!!
+    @endforelse
+
